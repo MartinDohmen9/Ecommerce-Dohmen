54535062b0a71c2fca152d859625fb49b1f9a2f3
MartinDohmen9 <martindohmen9@gmail.com>
2022-06-23 13:37:55 -0300

    enter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer";
 
 const App = () => {
   return (
+    <>
     <NavBar/>
+    <ItemListContainer greeting={'Bienvenido a Donuts Homero'}/>
+    </>
   )
 };
 
